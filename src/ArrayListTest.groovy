29bb6605638ba6c200e2f3101482740836e603c0
crimy <tmddnjs950@naver.com>
2023-03-25 22:27:15 +0900

    size, add, get test code

--- src/ArrayListTest.groovy
+++ src/ArrayListTest.groovy
@@ -1,6 +1,14 @@
 class ArrayListTest extends spock.lang.Specification {
 
     def "list test : add" (){
+        given:
+        ArrayList<String> testList = new ArrayList<>();
+        def text = "addSomething"
+        when:
+        testList.add("addSomething");
 
+        then:
+        testList.size() == 1;
+        testList.get(0) == text
     }
 }
